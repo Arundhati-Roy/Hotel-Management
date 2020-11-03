559dc016200e8571c1ce57460a37c3184ec7d984
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-11-03 12:34:22 +0530

    first commit

---
+++ HotelManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hotel reservation System!");
+        }
+    }
+}
